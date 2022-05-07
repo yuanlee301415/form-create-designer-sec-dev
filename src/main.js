0d4e2b2dd48b35eee47706d06c064f953a525074
yuanlee <yuanlee@163.com>
2022-05-07 10:52:28 +0800

    安装 form-create 依赖

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
+import ELEMENT from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import formCreate from '@form-create/element-ui';
+import FcDesigner from '@/components/form-designer';
 
 import App from './App.vue'
 import router from './router'
 import '@/assets/style.css'
 
 Vue.config.productionTip = false
+
+Vue.use(ELEMENT);
+Vue.use(formCreate);
+Vue.component('FcDesigner', FcDesigner);
 
 new Vue({
   el: '#app',
